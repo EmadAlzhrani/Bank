f610505698a76d116142176133c279c8d77b3b46
EmadAlzhrani <105259351+EmadAlzhrani@users.noreply.github.com>
2025-05-04 15:18:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showCardNumberPopup() {
+    document.getElementById('cardNumberPopup').style.display = 'block';
+  }
+  
+  function hideCardNumberPopup() {
+    document.getElementById('cardNumberPopup').style.display = 'none';
+  }
+  
